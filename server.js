dfaa1caec26fc184ab8bad718f546913ab79f337
RaceProUK <raceprouk@hotmail.co.uk>
2015-03-30 19:08:18 +0100

    Basically got rid of eslint errors

--- server.js
+++ server.js
@@ -1,37 +1,41 @@
-var http = require("http"),
-    url = require("url"),
-    path = require("path"),
-    fs = require("fs"),
-    port =  process.env.PORT || 8888;
+/*jslint node: true, indent: 4 */
+/* eslint-disable no-console */
+'use strict';
+var http = require('http'),
+    url = require('url'),
+    path = require('path'),
+    fs = require('fs');
+var port = process.env.PORT || 8888;
 
-http.createServer(function(request, response) {
+http.createServer(function (request, response) {
+    var uri = url.parse(request.url).pathname,
+        filename = path.join(process.cwd(), 'static', uri);
 
-  var uri = url.parse(request.url).pathname
-    , filename = path.join(process.cwd(), 'static', uri);
+    console.log(uri);
 
-  console.log(uri);
+    fs.exists(filename, function (exists) {
+        if (!exists) {
+            response.writeHead(404, {'Content-Type': 'text/plain'});
+            response.write('404 Not Found\n');
+            response.end();
+            return;
+        }
 
-  fs.exists(filename, function(exists) {
-    if(!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found\n");
-      response.end();
-      return;
-    }
+        if (fs.statSync(filename).isDirectory()) {
+            filename += '/index.html';
+        }
 
-    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+        fs.readFile(filename, 'binary', function (err, file) {
+            if (err) {
+                response.writeHead(500, {'Content-Type': 'text/plain'});
+                response.write(err + '\n');
+                response.end();
+                return;
+            }
 
-    fs.readFile(filename, "binary", function(err, file) {
-      if(err) {        
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
-        response.end();
-        return;
-      }
-
-      response.writeHead(200);
-      response.write(file, "binary");
-      response.end();
+            response.writeHead(200);
+            response.write(file, 'binary');
+            response.end();
+        });
     });
-  });
 }).listen(parseInt(port, 10));
