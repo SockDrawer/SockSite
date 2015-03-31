87ac63950ba35c34d36280c1df565eedee91aff5
onyx47 <bnemcic@gmail.com>
2015-03-31 19:04:06 +0000

    Add spin! Also, CSS improvements.

---
+++ static/scripts/spin.js
@@ -0,0 +1,27 @@
+$(function(){
+    
+    $('.btn').on('click', function(evt) {
+       
+        evt.preventDefault();
+       
+        $('div').each(function(index){
+            
+            var spinTime = Math.floor((Math.random() * 4000) + 1000);
+           
+            $(this).css({
+                '-webkit-animation-name': 'spin',
+                '-webkit-animation-duration': spinTime + 'ms',
+                '-webkit-animation-iteration-count': 'infinite',
+                '-webkit-animation-timing-function': 'linear',
+                '-moz-animation-name': 'spin',
+                '-moz-animation-duration': spinTime + 'ms',
+                '-moz-animation-iteration-count': 'infinite',
+                '-moz-animation-timing-function': 'linear',
+                '-ms-animation-name': 'spin',
+                '-ms-animation-duration': spinTime + 'ms',
+                '-ms-animation-iteration-count': 'infinite',
+                '-ms-animation-timing-function': 'linear'
+            });
+        });
+    });
+});
