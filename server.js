876f2ed66bbc5bb5e4d532bc239cc2fe3cd96da8
Accalia <Accalia.de.Elementia@gmail.com>
2015-04-01 01:16:40 +0000

    version 0.0.5

--- server.js
+++ server.js
@@ -1,19 +1,82 @@
 /*jslint node: true, indent: 4 */
 /* eslint-disable no-console */
 'use strict';
-var http = require('http'),
+var db = require('./database'),
+    config = require('./config.json'),
+    checks = require('./check'),
+    http = require('http'),
     url = require('url'),
     path = require('path'),
     fs = require('fs'),
     mustache = require('mustache');
-var port = process.env.PORT || 8888;
+var port = parseInt(process.env.PORT || 8888, 10),
+    ip = process.env.IP || undefined;
 
-http.createServer(function (request, response) {
-    var uri = url.parse(request.url).pathname,
-        filename = path.join(process.cwd(), 'static', uri);
+checks.start();
 
-    console.log(uri);
+function getData(callback) {
+    db.getRecentChecks(10 * 60 * 1000, function (err, data) {
+        if (err || !data) {
+            return callback(err || 'NO DATA');
+        }
+        var keys,
+            sum = 0,
+            result = {
+                version: config.version,
+                time: new Date().toISOString(),
+                checks: [],
+                summary: []
+            };
+        keys = Object.keys(data);
+        keys.sort();
+        keys.forEach(function (key) {
+            var time = [],
+                status = [],
+                polled = 0,
+                type = 'BAD',
+                check = {
+                    name: key,
+                    values: data[key].map(function (row) {
+                        polled = Math.max(0, row.checkedAt);
+                        time.push(row.responseTime);
+                        status.push(row.status);
+                        return {
+                            code: row.status,
+                            responseTime: row.responseTime,
+                            polledAt: row.checkedAt
+                        };
+                    })
+                };
+            time = time.reduce(function (a, b) {
+                return a + b;
+            }, 0) / time.length;
+            status = status.reduce(function (a, b) {
+                return a + b;
+            }, 0) / status.length;
+            if (status === 200) {
+                type = 'GREAT';
+            } else if (status < 300) {
+                type = 'OK';
+            }
+            sum += status;
+            result.summary.push({
+                name: key,
+                code: status,
+                response: type,
+                responseTime: time,
+                polledAt: polled
+            });
+            result.checks.push(check);
+        });
+        sum /= keys.length;
+        result.up = sum < 300;
+        result.percentage = sum;
+        callback(null, result);
+    });
+}
 
+
+function serveStatic(filename, response) {
     fs.exists(filename, function (exists) {
         if (!exists) {
             response.writeHead(404, {
@@ -39,30 +102,52 @@ http.createServer(function (request, response) {
             }
 
 
-            if (uri === '/') {
-                fs.readFile(path.join(process.cwd(), 'example.json'), function (err, data) {
-                    if (err) {
-                        response.writeHead(500, {
-                            'Content-Type': 'text/plain'
-                        });
-                        response.write(err + '\n');
-                        response.end();
-                        return;
-                    }
-                    try{
-                        data = JSON.parse(data);
-                    } catch (e) {
-                        data = {};
-                    }
-                    response.writeHead(200);
-                    response.write(mustache.render(file, data), 'binary');
-                    response.end();
-                });
-            } else {
-                response.writeHead(200);
-                response.write(file, 'binary');
-                response.end();
-            }
+            response.writeHead(200);
+            response.write(file, 'binary');
+            response.end();
         });
     });
-}).listen(parseInt(port, 10));
+}
+
+http.createServer(function (request, response) {
+    var uri = url.parse(request.url).pathname,
+        filename = path.join(process.cwd(), 'static', uri);
+
+    console.log(uri);
+    if (uri === '/' || uri === '/index.json') {
+        getData(function (err, data) {
+            if (err) {
+                response.writeHead(500, {
+                    'Content-Type': 'text/plain'
+                });
+                response.write(err + '\n');
+                response.end();
+                return;
+            }
+            data.host = request.headers.host;
+            response.writeHead(200);
+            if (uri === '/index.json') {
+                response.write(JSON.stringify(data), 'binary');
+                response.end();
+            } else {
+                fs.readFile(path.join(process.cwd(), 'static', 'index.html'),
+                    'binary',
+                    function (err2, file) {
+                        if (err2) {
+                            response.writeHead(500, {
+                                'Content-Type': 'text/plain'
+                            });
+                            response.write(err2 + '\n');
+                            response.end();
+                            return;
+                        }
+                        response.write(mustache.render(file, data), 'binary');
+                        response.end();
+                    });
+            }
+
+        });
+    } else {
+        serveStatic(filename, response);
+    }
+}).listen(port, ip);
