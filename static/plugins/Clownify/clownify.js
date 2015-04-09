a97b47d2a011b55d37ae01ff21894651ae343444
Yami <yamikuronue@gmail.com>
2015-04-09 16:40:53 -0400

    Not fully tested, but updated Clownify plugin

--- static/plugins/Clownify/clownify.js
+++ static/plugins/Clownify/clownify.js
@@ -4,13 +4,18 @@
 /* eslint-disable no-console */
 'use strict';
 $(function(){
+	var normalImage = "/static/images/trwtf.png";
+	var clownImage = "/static/images/trwtf_clown.png";
+	
     $('#vomitbutton').on('click', function(evt) {
         evt.preventDefault();
 		
 		if ($("#clownvomit_css").length ) {
 			$("#clownvomit_css").remove();
+			$("#trwtfimg").attr("src", normalImage);
 		} else {
-			 $('head').append('<link id="clownvomit_css" type="text/css" rel="stylesheet" media="screen" href="/static/styles/clownvomit.css">');
+			 $('head').append('<link id="clownvomit_css" type="text/css" rel="stylesheet" media="screen" href="/static/plugins/Clownify/clownvomit.css">');
+			 $("#trwtfimg").attr("src", clownImage);
 		}
 	})
 });
