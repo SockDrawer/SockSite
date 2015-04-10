cbfd974dfaf869d396be15cf3a61fe57f66b6f69
onyx47 <bnemcic@gmail.com>
2015-04-10 15:40:06 +0000

    Fix spin button to actually stop the spinning as well. If you can catch it.

--- static/scripts/spin.js
+++ static/scripts/spin.js
@@ -5,24 +5,19 @@ $(function(){
     $('#spin-button').on('click', function(evt) {
         evt.preventDefault();
 
-        $('div').each(function(){
-
-            var spinTime = Math.floor((Math.random() * 4000) + 1000);
-
-            $(this).css({
-                '-webkit-animation-name': 'spin',
-                '-webkit-animation-duration': spinTime + 'ms',
-                '-webkit-animation-iteration-count': 'infinite',
-                '-webkit-animation-timing-function': 'linear',
-                '-moz-animation-name': 'spin',
-                '-moz-animation-duration': spinTime + 'ms',
-                '-moz-animation-iteration-count': 'infinite',
-                '-moz-animation-timing-function': 'linear',
-                '-ms-animation-name': 'spin',
-                '-ms-animation-duration': spinTime + 'ms',
-                '-ms-animation-iteration-count': 'infinite',
-                '-ms-animation-timing-function': 'linear'
+        if($(this).closest('div').hasClass('spinning')) {
+            $('.spinning').removeClass('spinning');
+        } else {
+            $('div').each(function(){
+    
+                var spinTime = Math.floor((Math.random() * 4000) + 1000);
+    
+                $(this).css({
+                    '-webkit-animation-duration': spinTime + 'ms',
+                    '-moz-animation-duration': spinTime + 'ms',
+                    '-ms-animation-duration': spinTime + 'ms',
+                }).addClass('spinning');
             });
-        });
+        }
     });
 });
