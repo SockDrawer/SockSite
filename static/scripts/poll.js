6a52729b462bba1f90aef75087e3f8ce66bd3b76
onyx47 <bnemcic@gmail.com>
2015-04-18 17:17:14 +0000

    Add live updating to status and history tabs

---
+++ static/scripts/poll.js
@@ -0,0 +1,33 @@
+var statusTemplate;
+var historyTemplate;
+var graphTemplate;
+
+function pollForData() {
+    $.get('/index.json', {}, function(data) {
+        var statusRendered = Mustache.render(statusTemplate, data);
+        var historyRendered = Mustache.render(historyTemplate, data);
+        
+        $('#status').find('.table').html(statusRendered);
+        
+        $($.parseHTML(historyRendered)).find('.panel-collapse').each(function(){
+            $('#' + $(this).attr('id')).html($(this).html());
+        });
+
+    }, 'json');
+}
+
+$(function() {
+    $.get('/templates/status_table.html', {}, function(data) {
+        statusTemplate = data;
+    }, 'html');
+    
+    $.get('/templates/history_table.html', {}, function(data) {
+        historyTemplate = data;
+    }, 'html');
+    
+    $.get('/templates/graph.html', {}, function(data) {
+        graphTemplate = data;
+    }, 'html');
+    
+    setInterval(pollForData, 5000);
+});
