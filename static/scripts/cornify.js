f7934ebc3a9301f9f2dc2d62f9511535d8b0a29b
RaceProUK <raceprouk@hotmail.co.uk>
2015-04-19 14:27:22 +0100

    Cornify!

---
+++ static/scripts/cornify.js
@@ -0,0 +1,9 @@
+/*jslint indent: 4 */
+/* eslint-disable no-console */
+'use strict';
+$(function(){
+    $('#cornify-button').on('click', function(evt) {
+        evt.preventDefault();
+        cornify_add();
+    });
+});
