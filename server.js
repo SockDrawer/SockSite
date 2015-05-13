7d3689c8ad46f36dadf8fb2fc7c7b0e19cd82966
Accalia <Accalia.de.Elementia@gmail.com>
2015-05-13 01:31:04 +0000

    start documenting things.

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 /*jslint node: true, indent: 4 */
 'use strict';
+/**
+ * Main entry point to SockSite. Server starts here.
+ * @module server
+ * @author Accalia
+ * @license MIT
+ */
 
+
+// Make sure we get a stack trace on uncaught exception. 
+// we're not supposed to get those but just in case
 process.on('uncaughtException', function (err) {
     /*eslint-disable no-process-exit, no-console*/
     console.error(err.stack);
@@ -25,12 +34,19 @@ require('./graph');
 
 checks.start();
 
+/**
+ * Handler for HTTP requests. All HTTP requests start here
+ */
 function handler(request, response) {
     var uri = url.parse(request.url).pathname;
 
+    // Log request
     /* eslint-disable no-console */
     console.log(uri);
     /* eslint-enable no-console */
+    
+    // Check the paths known to router to render response.
+    // Response is handled by the renderer. we only care about is it handled
     var rendered = router.paths.some(function (point) {
         var res = point.path.test(uri);
         if (res) {
@@ -38,12 +54,17 @@ function handler(request, response) {
         }
         return res;
     });
+    // Render 404 if no route found
     if (!rendered) {
         render.render404Error(response);
     }
     return;
 
 }
+
+// Kick off the initial cache build.
+// Start the HTTP server in the callback to this so initial cache is 
+// loaded first
 cache.buildCache(function (err) {
     /*eslint-disable no-console */
     if (err) {
@@ -54,25 +75,32 @@ cache.buildCache(function (err) {
     /*eslint-neable no-console */
 });
 
+//Export websockets socket for other modules
 exports.io = io;
 
+// Set up per connection socket events
 io.on('connection', function (socket) {
+    // Send the date on connection
     socket.emit('connected', Date.now());
+    // When client emits `getdata` reply with current summary
     socket.on('getdata', function (callback) {
         if (!cache.summary) {
             return callback('E_NO_DATA');
         }
         callback(null, cache.summary);
     });
+    // render templates server side on request.
     socket.on('render', function (template, callback) {
         if (!cache.summary) {
             return callback('E_NO_DATA');
         }
         render.formatHTML(cache.summary, template, callback);
     });
+    // send client currently loaded templates on request
     socket.on('gettemplates', function (callback) {
         callback(null, cache.templates);
     });
+    // send client graph data on req
     socket.on('getgraphs', function (callback) {
         callback(null, {
             'timings': cache.summary.getTimeChart(),
@@ -80,7 +108,7 @@ io.on('connection', function (socket) {
         });
     });
 });
-
+//Emit heartbeat event regularly
 async.forever(function (next) {
     io.emit('heartbeat', Date.now());
     setTimeout(next, 30 * 1000);
