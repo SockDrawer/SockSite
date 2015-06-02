4026d020327ef2e2c4ccb987a9558552bbe9085e
Accalia <Accalia.de.Elementia@gmail.com>
2015-06-02 01:20:21 +0000

    docs

--- utility.js
+++ utility.js
@@ -2,6 +2,25 @@
 var async = require('async');
 var config = require('./config.json');
 
+/**
+ * @typedef ServerCheck
+ * @type {object}
+ * @property {string} checkName Name of the checked endpoint
+ * @property {number} checkId Id of the checkName.
+ * @property {number} responseCode HTTP response code from the check
+ * @property {number} responseTime Response time of the check in seconds
+ * @property {number} checkedAt UNIX timestamp of the check
+ *
+ */
+
+/**
+ * turns an number into text. return value will be selected from the input by selecting the bigest key that is smaller
+ * than the number. if the selected value is an array a random value of the array is selected.
+ *
+ * @param {number} value Value to flavorize
+ * @param {Object<number, string>|Object<number, string[]>} arr flavors that can be applied
+ * @returns {string} flavor selected based on value
+ */
 exports.getFlavor = function getFlavor(value, arr) {
     var key = Object.keys(arr).filter(function (a) {
             return a <= value;
@@ -15,7 +34,12 @@ exports.getFlavor = function getFlavor(value, arr) {
     return res;
 };
 
-
+/**
+ * Calculate DiscoApdex score from a remote server check.
+ *
+ * @param {ServerCheck} check the Check to calculate the score of
+ * @returns {number} DiscoApdex score for the check
+ */
 exports.getScore = function getScore(check) {
     var code = check.responseCode,
         time = check.responseTime;
@@ -27,20 +51,52 @@ exports.getScore = function getScore(check) {
     return 100;
 };
 
+/**
+ * Get an array of numbers from 0 to num (exclusive)
+ *
+ * @param {number} num Endpoint of the range
+ * @returns {number[]} array of [0-num)
+ */
 exports.range = function range(num) {
     return Array.apply(null, Array(num)).map(function (_, i) {
         return i;
     });
 };
 
+/**
+ * Round a number to a specified number of decimal points. based on example from MDN
+ *
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round}
+ * @param {number} num number to round
+ * @param {integer} places number of decimal places
+ * @returns {number} num rounded to places decimal places
+ */
 exports.round = function round(num, places) {
-    if (!places) {
-        places = 0;
+    // If the exp is undefined or zero...
+    if (typeof places === 'undefined' || +places === 0) {
+        return Math.round(num);
     }
-    places = Math.pow(10, places);
-    return Math.round(num * places) / places;
+    num = +num;
+    places = +places;
+    // If the value is not a number or the exp is not an integer...
+    if (isNaN(num) || !(typeof places === 'number' && places % 1 === 0)) {
+        return NaN;
+    }
+    // Shift
+    num = num.toString().split('e');
+    num = Math.round(+(num[0] + 'e' + (num[1] ? (+num[1] - places) : -places)));
+    // Shift back
+    num = num.toString().split('e');
+    return +(num[0] + 'e' + (num[1] ? (+num[1] + places) : places));
 };
 
+/**
+ * Sum the values in an input array, optionally mapped through a map function.
+ *
+ * @param {*[]} arr Array of things to sum
+ * @param {function} [mapper] Mapper of objects to number to apply to the input array
+ * @returns {number} the sum
+ */
 exports.sum = function sum(arr, mapper) {
     if (!mapper || !mapper.call) {
         mapper = function mapper(data) {
@@ -52,6 +108,13 @@ exports.sum = function sum(arr, mapper) {
     }, 0);
 };
 
+/**
+ * Average the values in an input array, optionally mapped through a map function.
+ *
+ * @param {*[]} arr Array of things to average
+ * @param {function} [mapper] Mapper of objects to number to apply to the input array
+ * @returns {number} the average
+ */
 exports.average = function average(arr, mapper) {
     if (!mapper || !mapper.call) {
         mapper = function mapper(data) {
@@ -61,6 +124,13 @@ exports.average = function average(arr, mapper) {
     return exports.sum(arr, mapper) / arr.length;
 };
 
+/**
+ * Get the maximum of the values in an input array, optionally mapped through a map function.
+ *
+ * @param {*[]} arr Array of things to get the maximum
+ * @param {function} [mapper] Mapper of objects to number to apply to the input array
+ * @returns {number} the maximum value
+ */
 exports.max = function max(arr, mapper) {
     if (!mapper || !mapper.call) {
         mapper = function mapper(data) {
@@ -70,6 +140,10 @@ exports.max = function max(arr, mapper) {
     return Math.max(arr.map(mapper));
 };
 
+/**
+ * Get a function to calculate Scores for server checks, materialize "overall" points as they become available
+ * @returns {function} row formatter
+ */
 exports.formattedRowGenerator = function formattedRowGenerator() {
     function format(row) {
         row.score = exports.getScore(row);
@@ -77,26 +151,33 @@ exports.formattedRowGenerator = function formattedRowGenerator() {
         row.polledAt = new Date(row.checkedAt).toGMTString();
     }
     var avg = [];
+    /**
+     * Calculate Scores for server checks, materialize "overall" points as they become available
+     */
     return function (row, callback) {
         var res = [row];
         avg.unshift(row);
+        // Truncate average according to the rules for overall scores
+        avg = exports.truncateData(avg, function (r) {
+            return r.checkedAt >= (row.checkedAt - config.scorePeriod * 1000);
+        }, config.scoreEntries);
         format(row);
-        if (avg.length === config.checks.length) {
-            var overall = {
-                checkName: 'overall',
-                checkId: -1,
-                responseCode: exports.round(exports.max(avg, function (n) {
-                    return n.responseCode;
-                })),
-                responseTime: exports.round(exports.average(avg, function (n) {
-                    return n.responseTime;
-                }), 3),
-                checkedAt: avg[0].checkedAt
-            };
-            format(overall);
-            res.push(overall);
-            avg.pop();
-        }
+        // Generate the overall score. This will be jittery for the first few entries formatted but should settle
+        // quickly
+        var overall = {
+            checkName: 'overall',
+            checkId: -1,
+            responseCode: exports.round(exports.max(avg, function (n) {
+                return n.responseCode;
+            })),
+            responseTime: exports.round(exports.average(avg, function (n) {
+                return n.responseTime;
+            }), 3),
+            checkedAt: avg[0].checkedAt
+        };
+        format(overall);
+        res.push(overall);
+        // Make sure we don't block the server. schedule the callback for the next tick
         process.nextTick(function () {
             callback(null, res);
         });
@@ -122,9 +203,15 @@ exports.parseData = function parseData(data, formatter, callback) {
     });
 };
 
-
+/**
+ * Truncate a data array, applying a filter and respecting a minimum number of entries
+ *
+ * @param {*[]} data array to truncate
+ * @param {function} [filter] filter to apply to the data.
+ * @param {integer} minimum minimum number of data entries.
+ */
 exports.truncateData = function truncateData(data, filter, minimum) {
-    if (!data){
+    if (!data) {
         return [];
     }
     if (typeof filter === 'number') {
@@ -140,6 +227,14 @@ exports.truncateData = function truncateData(data, filter, minimum) {
     return res;
 };
 
+/**
+ * Average the values in an input array and round to a specified number of decimal places, mapped through a map function
+ *
+ * @param {*[]} data Array of things to average
+ * @param {function} reduce Mapper of objects to number to apply to the input array
+ * @param {integer} places number of decimal places to round to
+ * @returns {number} the average, rounded to `places` decimal places
+ */
 exports.roundAverage = function roundAverage(data, reduce, places) {
     return exports.round(exports.average(data, reduce), places);
 };
