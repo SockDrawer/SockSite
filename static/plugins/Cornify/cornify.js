05f9a1a4fbaf51cd40db569c17ec9c7e50ebe7c0
RaceProUK <raceprouk@hotmail.co.uk>
2015-06-03 22:50:12 +0100

    Add cornify to random Discodefinition paragraph
    At some point, I may narrow this down to an individual word; until then,
    this'll do

--- static/plugins/Cornify/cornify.js
+++ static/plugins/Cornify/cornify.js
@@ -1,14 +1,17 @@
-//<link type="text/css" rel="stylesheet" media="screen" href="/static/styles/clownvomit.css">
-
 /*jslint indent: 4 */
 /* eslint-disable no-console */
 'use strict';
 
 document.cookie = 'cornify=0;expires=' + new Date().toUTCString();
 
+function cornify(evt) {
+    evt.preventDefault();
+    cornify_add();
+}
+
 $(function () {
-    $('#cornifybutton').on('click', function (evt) {
-        evt.preventDefault();
-        cornify_add();
-    });
+    var blocks = $('div.quote p');
+    var block = blocks[Math.floor(Math.random() * blocks.length)];
+    $(block).on('click', cornify);
+    $('#cornifybutton').on('click', cornify);
 });
