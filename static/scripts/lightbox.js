2d4de3a6138a68038b8543f996fb749eedd446c5
onyx47 <bnemcic@gmail.com>
2015-06-05 22:32:10 +0000

    Open embedded images in ligthboxes (as per #26)

---
+++ static/scripts/lightbox.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.quote img').each(function() {
+        if($(this).parent('a').length == 0) {
+            $(this).wrap('<a></a>');
+            $(this).parent('a').attr('href', $(this).attr('src'));
+        }
+        $(this).parent('a').data('featherlight', 'image');
+        $(this).parent('a').featherlight();
+    });
+});
