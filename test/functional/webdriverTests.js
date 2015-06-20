8fe774c30fb4d6f16d25d5d57eff3a4405e6c6d3
Yami <yamikuronue@gmail.com>
2015-06-20 19:15:06 -0400

    Initial test passes!

--- test/functional/webdriverTests.js
+++ test/functional/webdriverTests.js
@@ -1,40 +1,75 @@
-var webdriverio = require('webdriverio');
-var server = require('../../server.js')
+var webdriver = require('selenium-webdriver');
+var SeleniumServer = require('selenium-webdriver/remote').SeleniumServer;
+var socksite = require('../../server.js')
 var siteURL = 'http://localhost:8888'; 
 
 var chai        = require('chai'),
     assert      = chai.assert;
 
-var webdriveroptions = { 
-    desiredCapabilities: { 
-        browserName: 'firefox' 
-    }, 
-    port: 4444
-}; 
+
+/*Test data*/
+ var goodData = {
+	up: true,
+	score: 100,
+	code: "GREAT",
+	status: "This is a good status",
+	flavor: "Flavor is delicious!",
+	readonly: false,
+	global_notice: "",
+	global_notice_text: "",
+	summary: [
+		{
+			name: "overall",
+			response: "GREAT",
+			responseCode: 200,
+			responseScore: 100,
+			responseTime: 0.11,
+			polledAt: "Sat, 20 Jun 2015 21:37:36 GMT",
+			checkIndex: -1
+		}
+	]
+};
 
 describe('Socksite', function(){ 
     var browser = {}; 
-	 this.timeout(500000);
+	 this.timeout(20000);
+	 var driver;
 
     before('init browser session', function(done){
-		server.start(8888, 'localhost', function() {
-			browser = webdriverio.remote(webdriveroptions); 
-			browser.init(done); 
+		
+		socksite.start(8888, 'localhost', function() {
+			
+			var selserver = new SeleniumServer("../selenium/selenium-server-standalone-2.46.0.jar", {
+				port: 4446
+			});
+			
+			process.env.SOCKDEV = true;
+			
+			driver = new webdriver.Builder().
+					usingServer('http://localhost:4444/wd/hub').
+					withCapabilities(webdriver.Capabilities.firefox()).
+					build();
+			driver.get("localhost:8888").then(function() {
+						console.log("Done with startuip");
+						done();		
+					});
+	
 		})
-
     }); 
 
     it('should be running', function(done) {
-		browser 
-            .url(siteURL)
-			 .title(function(err, res) {
-				assert.equal(undefined, err);
-                assert.strictEqual(res.value,'Is it just me or server cooties?');
-				done();
-			  })
+		driver.getTitle().then(function(title) {
+			assert.strictEqual(title,'Is it just me or server cooties?',"Should have the right title");
+			done();
+		 });
+	});
+	
+	it('should report good status', function(done) {
+		done();
 	});
 
-    after('end browser session', function(done){ 
-        browser.endAll(done); 
+    after('end browser session', function(){ 
+        if (driver) driver.close();
+		driver = undefined;
     }); 
 });
