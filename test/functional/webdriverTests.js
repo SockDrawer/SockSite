efd829031910b3e3dfd464905c205df7ba5eb472
Yami <yamikuronue@gmail.com>
2015-06-20 21:06:07 -0400

    Making a suite out of TRWTF tests

--- test/functional/webdriverTests.js
+++ test/functional/webdriverTests.js
@@ -39,19 +39,7 @@ describe('Socksite', function(){
 			done();
 		 });
 	});
-	
-	it('should report good status with the Is You image', function(done) {
-		cache.summary = testData.goodData;
-		
-		driver.get("localhost:8888").then(function() {
-			driver.findElement(webdriver.By.css("#header-image-wrapper img")).getAttribute("src").then(function(value) {
-				assert.match( value,/isyou\.png/, "Image should say 'Is you'");
-				done();
-			})
-		});
-		
-	});
-	
+
 	it('should report the correct flavor text', function(done) {
 		cache.summary = testData.goodData;
 		
@@ -75,17 +63,62 @@ describe('Socksite', function(){
 		});
 	});
 	
-	it('should report bad status with the Is Discourse image', function(done) {
-		cache.summary = testData.badData;
-		
-		driver.get("localhost:8888").then(function() {
-			driver.findElement(webdriver.By.css("#header-image-wrapper img")).getAttribute("src").then(function(value) {
-				assert.match( value,/isdiscourse\.png/, "Image should say 'Is discourse'");
-				done();
-			})
+	describe('TRWTF', function() {
+		it('is You when status is "Great"', function(done) {
+			cache.summary = testData.greatData;
+			
+			driver.get("localhost:8888").then(function() {
+				driver.findElement(webdriver.By.css("#header-image-wrapper img")).getAttribute("src").then(function(value) {
+					assert.match( value,/isyou\.png/, "Image should say 'Is you'");
+					done();
+				})
+			});
+		});
+	
+		it('is You when status is "Good"', function(done) {
+			cache.summary = testData.goodData;
+			
+			driver.get("localhost:8888").then(function() {
+				driver.findElement(webdriver.By.css("#header-image-wrapper img")).getAttribute("src").then(function(value) {
+					assert.match( value,/isyou\.png/, "Image should say 'Is you'");
+					done();
+				})
+			});
 		});
 		
-	});
+		it('is Discourse when status is "OK"', function(done) {
+			cache.summary = testData.okData;
+			
+			driver.get("localhost:8888").then(function() {
+				driver.findElement(webdriver.By.css("#header-image-wrapper img")).getAttribute("src").then(function(value) {
+					assert.match( value,/isdiscourse\.png/, "Image should say 'Is discourse'");
+					done();
+				})
+			});
+		});
+		
+		it('is Discourse when status is "Bad"', function(done) {
+			cache.summary = testData.badData;
+			
+			driver.get("localhost:8888").then(function() {
+				driver.findElement(webdriver.By.css("#header-image-wrapper img")).getAttribute("src").then(function(value) {
+					assert.match( value,/isdiscourse\.png/, "Image should say 'Is discourse'");
+					done();
+				})
+			});
+		});
+		
+		it('is Discourse when status is "Offline"', function(done) {
+			cache.summary = testData.offlineData;
+			
+			driver.get("localhost:8888").then(function() {
+				driver.findElement(webdriver.By.css("#header-image-wrapper img")).getAttribute("src").then(function(value) {
+					assert.match( value,/isdiscourse\.png/, "Image should say 'Is discourse'");
+					done();
+				})
+			});
+		});
+	})
 
     after('end browser session', function(done){ 
         driver.close().then(done);
