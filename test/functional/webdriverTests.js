cf5517c0f6079ea023d57c82890cda48aaabe763
Yami <yamikuronue@gmail.com>
2015-06-20 20:44:21 -0400

    Test for IsDiscourse

--- test/functional/webdriverTests.js
+++ test/functional/webdriverTests.js
@@ -34,6 +34,28 @@ var chai        = require('chai'),
 	]
 };
 
+var badData = {
+	up: false,
+	score: 25,
+	code: "BAD",
+	status: "This is a bad status",
+	flavor: "Flavor is muy mal!",
+	readonly: false,
+	global_notice: "",
+	global_notice_text: "",
+	summary: [
+		{
+			name: "overall",
+			response: "BAD",
+			responseCode: 504,
+			responseScore: 25,
+			responseTime: 15,
+			polledAt: "Sat, 20 Jun 2015 21:37:36 GMT",
+			checkIndex: -1
+		}
+	]
+};
+
 describe('Socksite', function(){ 
     var browser = {}; 
 	 this.timeout(40000);
@@ -93,6 +115,17 @@ describe('Socksite', function(){
 		driver.get("localhost:8888").then(function() {
 			driver.findElement(webdriver.By.id("statusText")).getText().then(function(value) {
 				assert.equal( value,goodData.status, "Status text should be output");
+				done();
+			})
+		});
+	});
+	
+	it('should report bad status with the Is Discourse image', function(done) {
+		cache.summary = badData;
+		
+		driver.get("localhost:8888").then(function() {
+			driver.findElement(webdriver.By.css("#header-image-wrapper img")).getAttribute("src").then(function(value) {
+				assert.match( value,/isdiscourse\.png/, "Image should say 'Is discourse'");
 				done();
 			})
 		});
